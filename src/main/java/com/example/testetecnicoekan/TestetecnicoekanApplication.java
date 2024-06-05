ac43a09c55ede957e20cf24f230d56db3f5b18b2
Henrique Santos da Silva <45521656+Henrique-Santos-da-Silva@users.noreply.github.com>
2024-06-04 21:15:17 -0300

    feat: Commit Inicial

---
+++ src/main/java/com/example/testetecnicoekan/TestetecnicoekanApplication.java
@@ -0,0 +1,13 @@
+package com.example.testetecnicoekan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestetecnicoekanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestetecnicoekanApplication.class, args);
+    }
+
+}
